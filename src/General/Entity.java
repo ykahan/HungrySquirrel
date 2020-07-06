0f42f46f3da7039c788ad20cac5877c76baab581
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2020-07-06 11:05:04 +0300

    Created put() method and supporting isWall() method

--- src/General/Entity.java
+++ src/General/Entity.java
@@ -1,4 +1,18 @@
 package General;
 
-public class Entity {
+public abstract class Entity {
+    private char symbol;
+    private int row;
+    private int column;
+
+    public Entity put(int newRow, int newColumn){
+        Entity oldEntity = Maze.getEntity(newRow, newColumn);
+        boolean isWall = isWall(oldEntity);
+        if(!isWall) Maze.setEntity(this, newRow, newColumn);
+        return oldEntity;
+    }
+
+    private boolean isWall(Entity entity){
+        return entity.getClass().getName().contentEquals("Wall");
+    }
 }
