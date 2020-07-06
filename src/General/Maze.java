a540f1692c309d3e1df1591d602d64e046302c82
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2020-07-06 21:58:32 +0300

    Created class variables and create() method

--- src/General/Maze.java
+++ src/General/Maze.java
@@ -1,4 +1,30 @@
 package General;
 
+import Walls.Wall;
+
+import java.util.Scanner;
+
 public class Maze {
+    private static final int maxRow = 20;
+    private static final int maxColumn = 50;
+    private static Entity[][] maze;
+
+    public static void create(String filename){
+        Scanner scanner = new Scanner(filename);
+        try{
+            for(int row = 0; row < maxRow; row++){
+                char[] chars = scanner.nextLine().toCharArray();
+                for(int column = 0; column < maxColumn; column++){
+                    Entity entity = null;
+                    if(chars[row] == '*') entity = new Wall();
+
+                    maze[row][column] = entity;
+                }
+            }
+        } catch(Exception e){
+            System.out.println(e.toString());
+        }
+        scanner.close();
+    }
+
 }
