55970af073e0c44de4b9ff2c1fe8485836fd7675
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2020-07-19 17:59:56 +0300

    Createst jUnit tests for create() method

---
+++ src/General/MazeTest.java
@@ -0,0 +1,37 @@
+package General;
+
+import Walls.Wall;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MazeTest {
+
+    @Test
+    void createMakesWalls() {
+        Maze maze = new Maze();
+        assertEquals(maze.getEntity(0, 0).getClass().getName(), "Walls.Wall");
+    }
+
+    @Test
+    void createMakesNulls(){
+        Maze maze = new Maze();
+        assertNull(maze.getEntity(7, 3), "'Error: Entity not null");
+    }
+
+    @Test
+    void getEntity() {
+    }
+
+    @Test
+    void setEntity() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void display() {
+    }
+}
