5589f17c62ba78c009f8920f553bfe205f17bdbc
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2020-07-20 15:32:21 +0300

    Added Messages class

---
+++ src/Messages/Messages.java
@@ -0,0 +1,19 @@
+package Messages;
+
+public class Messages {
+    public static void invalidInput(){
+        System.out.println("Invalid input.  Try again.");
+    }
+
+    public static void getRow(){
+        System.out.println("Enter row:");
+    }
+
+    public static void getColumn(){
+        System.out.println("Enter column:");
+    }
+
+    public static void locationInvalid(){
+        System.out.println("Location not available.  Please try again.");
+    }
+}
