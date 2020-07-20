1ca8dc6e2488d201501d124d66cd10a9c5033905
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2020-07-20 21:15:57 +0300

    Fixed paramaterless constructor to deal with non-integer inputs

--- src/Pixies/Squirrel.java
+++ src/Pixies/Squirrel.java
@@ -7,68 +7,75 @@ import General.OpenSpace;
 import Walls.Wall;
 
 import java.util.Scanner;
+
 import Messages.Messages;
 
-public class Squirrel implements Movable{
+public class Squirrel implements Movable {
     private Location location;
     private Scanner scanner = new Scanner(System.in);
     private Maze maze;
 
-    public Squirrel(int row, int column){
-       location = new Location(row, column);
-       maze = new Maze();
+    public static void main(String[] args) {
+        Squirrel sq = new Squirrel();
     }
 
-    public Squirrel(){
-        boolean foundAvailableLoc = false;
-        int row = -1;
-        int column = -1;
-        while(!foundAvailableLoc){
-            Messages.getRow();
-            try{row = Integer.parseInt(scanner.nextLine());}
-            catch(Exception e) {
-                Messages.invalidInput();
-                continue;
-            }
-            Messages.getColumn();
-            column = Integer.parseInt(scanner.nextLine());
-            foundAvailableLoc = locAvailable(row, column);
-            if(!foundAvailableLoc) Messages.locationInvalid();
-        }
+    public Squirrel(int row, int column) {
         location = new Location(row, column);
         maze = new Maze();
     }
 
-    public void setLocation(int row, int column){
+    public Squirrel() {
+        maze = new Maze();
+        boolean foundAvailableLoc = false;
+        int row = -1;
+        int column = -1;
+        while (!foundAvailableLoc) {
+            if (row == -1) {
+                Messages.getRow();
+                try {
+                    row = Integer.parseInt(scanner.nextLine());
+                } catch (Exception e) {
+                    Messages.invalidInput();
+                }
+            }
+            Messages.getColumn();
+            try {
+                column = Integer.parseInt(scanner.nextLine());
+            } catch (Exception e) {
+                Messages.invalidInput();
+            }
+            foundAvailableLoc = locAvailable(row, column);
+            if (!foundAvailableLoc) Messages.locationInvalid();
+        }
+        location = new Location(row, column);
+    }
+
+    public void setLocation(int row, int column) {
         this.location.setRow(row);
         this.location.setColumn(column);
     }
 
-    public Maze getMaze(){
+    public Maze getMaze() {
         return this.maze;
     }
 
-    public static void main(String[] args) {
-        Maze testMaze = new Maze();
-    }
-
-    public boolean locAvailable(int row, int column){
+    public boolean locAvailable(int row, int column) {
         if (locIsOutsideMaze(row, column)) return false;
 
         Entity entity = maze.getEntity(row, column);
         return !(entity instanceof Wall);
     }
 
-    public boolean locNotAvailable(int row, int column){
-        if(locIsOutsideMaze(row, column)) return true;
+    public boolean locNotAvailable(int row, int column) {
+        if (locIsOutsideMaze(row, column)) return true;
 
         Entity entity = maze.getEntity(row, column);
         return entity instanceof Wall;
     }
 
     private boolean locIsOutsideMaze(int row, int column) {
-        if(row < 0 || column < 0) return true;
-        if(row > maze.getMaxRow() || column > maze.getMaxColumn()) return true;
+        if (row < 0 || column < 0) return true;
+        if (row > maze.getMaxRow() || column > maze.getMaxColumn()) return true;
         return false;
     }
 }
