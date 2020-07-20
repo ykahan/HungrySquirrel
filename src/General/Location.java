626d8bbb78994eb9261f115b6be0208598500b74
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2020-07-20 14:49:11 +0300

    Added Location class

--- src/General/Location.java
+++ src/General/Location.java
@@ -1,4 +1,35 @@
 package General;
 
 public class Location {
+    private int row;
+    private int column;
+
+    public Location(int row, int column){
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow(){
+        return row;
+    }
+
+    public int getColumn(){
+        return column;
+    }
+
+    public void incrementRow(){
+        row++;
+    }
+
+    public void decrementRow(){
+        row--;
+    }
+
+    public void incrementColumn(){
+        column++;
+    }
+
+    public void decrementColumn(){
+        column--;
+    }
 }
